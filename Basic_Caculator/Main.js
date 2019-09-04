c27e27167cf405c632588c0666916a8a3505dce1
duyduong65 <tg.bluesky66@gmail.com>
2019-09-04 08:39:34 +0700

    final_PGM caculator

--- Basic_Caculator/Main.js
+++ Basic_Caculator/Main.js
@@ -1,11 +1,16 @@
-
-function inputHere(abc) {
-    document.getElementById("inputNumbers").value = abc;
-
+function displayValue(value) {
+    document.getElementById("inputNumbers").value += value;
 }
 
 
-
 function tinhtoan() {
+    let ketqua = document.getElementById("inputNumbers").value;
+    result = eval(ketqua);
+    document.getElementById("inputNumbers").value = result;
+}
 
+function del_1() {
+    let del = document.getElementById("inputNumbers").value;
+    result = del.substring(0, del.length - 1);
+    document.getElementById("inputNumbers").value = result;
 }
