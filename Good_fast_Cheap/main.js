a37d423b1b8a0ad51c732deeb26fc9cb29307aa1
duyduong65 <tg.bluesky66@gmail.com>
2019-09-05 22:13:02 +0700

    update good fast cheap

--- Good_fast_Cheap/main.js
+++ Good_fast_Cheap/main.js
@@ -1,15 +1,36 @@
-let good = document.getElementById('good') ;
+let good = document.getElementById('good');
 let fast = document.getElementById('fast');
 let cheap = document.getElementById('cheap');
 
 function changeChecked(eval) {
     switch (eval) {
         case 'good' :
-            if (good && fast) {
-                cheap = false;
-            } else if (good && cheap) {
-                fast = false;
+            if (good.checked && fast.checked) {
+                cheap.checked = false;
+            } else if (fast.checked && cheap.checked) {
+                good.checked = false;
+            } else if (good.checked && cheap.checked) {
+                fast.checked = false;
             }
+            break;
+        case "fast":
+            if (fast.checked && cheap.checked) {
+                good.checked = false;
+            } else if (cheap.checked && good.checked) {
+                fast.checked = false;
+            } else if (good.checked && fast.checked) {
+                cheap.checked = false;
+            }
+            break;
+        case "cheap" :
+            if (cheap.checked && good.checked) {
+                fast.checked = false;
+            } else if (cheap.checked && fast.checked) {
+                good.checked = false;
+            } else if (good.checked && fast.checked) {
+                cheap.checked = false;
+            }
+            break;
     }
 }
 
