e76e7ae1b8dd48e47f1a3be5b913259eb0bc69db
duyduong65 <tg.bluesky66@gmail.com>
2019-09-11 12:42:13 +0700

    add_T-gọi và tạo hàm

--- Good_fast_Cheap/main.js
+++ Good_fast_Cheap/main.js
@@ -7,22 +7,16 @@ function changeChecked(eval) {
         case 'good' :
             if (good.checked && fast.checked) {
                 cheap.checked = false;
-            } else if (fast.checked && cheap.checked) {
-                good.checked = false;
             }
             break;
-        case "fast":
+        case "fast" :
             if (fast.checked && cheap.checked) {
                 good.checked = false;
-            } else if (cheap.checked && good.checked) {
-                fast.checked = false;
             }
             break;
         case "cheap" :
             if (cheap.checked && good.checked) {
                 fast.checked = false;
-            } else if (cheap.checked && fast.checked) {
-                good.checked = false;
             }
             break;
     }
